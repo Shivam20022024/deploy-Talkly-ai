4fbdf67ac39f8f996c5fb2217da327d850365e14
Shivam Kumar <shivam279.hitece2020@gmail.com>
2026-08-21 20:23:15 +0530

    chore: Update CORS to allow production IP

--- backend/server.py
+++ backend/server.py
@@ -55,7 +55,12 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://10.32.118.192:3000", "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000", 
+        "http://52.220.153.141:3000",
+        "https://talkly.novalantis.ai"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
